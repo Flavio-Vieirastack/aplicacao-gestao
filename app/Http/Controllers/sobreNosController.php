114faf127423c68c7d5ff9f0144d110a697f7695
Flavio <flavioextreme1@hotmail.com>
2021-07-11 13:00:00 -0300

    master

---
+++ app/Http/Controllers/sobreNosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class sobreNosController extends Controller
+{
+    public function sobreNos () {
+
+        echo 'Sobre via controller';
+
+    }
+}
